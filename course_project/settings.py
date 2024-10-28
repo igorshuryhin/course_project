51a6ee13370d11346e0827522018cc009d79dab3
Lastochka1337 <shurigin.igor12@gmail.com>
2024-10-28 02:37:39 +0200

    add allowed_hosts

--- course_project/settings.py
+++ course_project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-*krp*b+97usdxd1d+i!*&l65k!re=y&(=b_+cpy=^^w6bm5(!=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 # Application definition
 
