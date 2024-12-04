0127ec4233cbdde3194d51ad0ee0cab5760c2e61
Lastochka1337 <shurigin.igor12@gmail.com>
2024-12-04 17:43:57 +0200

    edit services.py

--- google_sheets/services.py
+++ google_sheets/services.py
@@ -25,8 +25,8 @@ credentials = ServiceAccountCredentials.from_json_keyfile_dict(
         "private_key": private_key,
         "client_email": client_email,
         "client_id": client_id,
-        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_url": "https://accounts.google.com/o/oauth2/auth",
+        "token_url": "https://oauth2.googleapis.com/token",
         "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
         "client_x509_cert_url": f"https://www.googleapis.com/robot/v1/metadata/x509/{client_email}"
     },
