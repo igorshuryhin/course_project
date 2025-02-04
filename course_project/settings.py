0b290b7c9d6ecf54a8dfd17566246bc7865ea78e
igorshurygin <shurigin.igor12@gmail.com>
2025-02-05 00:18:35 +0200

    add AWS S3 integration

--- course_project/settings.py
+++ course_project/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'django_filters',
     "graphene_django",
+    'storages',
     # All-auth
     'allauth',
     'allauth.account',
@@ -251,5 +252,22 @@ LOGGING = {
             'level': 'DEBUG',
             'handlers': ['console'],
         }
+    }
+}
+
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_FILE_OVERWRITE = False
+
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+    },
+
+    "staticfiles": {
+        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
     }
 }
